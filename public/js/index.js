cce3bb3ff4600bbb814035b014106076e33e7221
ebubekeyz <ebubeofforjoe@gmail.com>
2023-11-08 07:37:20 +0100

    third commit

--- public/js/index.js
+++ public/js/index.js
@@ -61,6 +61,35 @@ window.addEventListener('DOMContentLoaded', async() => {
             }
         })
 
+        const insuranceFilter = article.filter((news) => {
+            if(news.category === 'insurance'){
+                return news
+            }
+        })
+
+        const productFilter = article.filter((news) => {
+            if(news.category === 'products'){
+                return news
+            }
+        })
+
+        const fintechFilter = article.filter((news) => {
+            if(news.category === 'fintech'){
+                return news
+            }
+        })
+
+        const eventsFilter = article.filter((news) => {
+            if(news.category === 'events'){
+                return news
+            }
+        })
+
+        const foodbankFilter = article.filter((news) => {
+            if(news.category === 'foodbank'){
+                return news
+            }
+        })
 
 
         const newsLength = newsFilter.length - 1
@@ -68,10 +97,25 @@ window.addEventListener('DOMContentLoaded', async() => {
 
         const moneyLength = moneyFilter.length - 1
         const moneyLength2 = moneyFilter.length - 2
-        console.log(moneyFilter[moneyLength])
+
+        const insuranceLength = insuranceFilter.length - 1
+        const insuranceLength2 = insuranceFilter.length - 2
+
+        const foodbankLength = foodbankFilter.length - 1
+        const foodbankLength2 = foodbankFilter.length - 2
+
+        const fintechLength = fintechFilter.length - 1
+        const fintechLength2 = fintechFilter.length - 2
+
+        const eventsLength = eventsFilter.length - 1
+        const eventsLength2 = eventsFilter.length - 2
 
         const mainCenterDOM = document.querySelector('.main-center')
         const moneyDOM = document.querySelector('#make-money')
+        const insuranceDOM = document.querySelector('#insurance')
+        const fintechDOM = document.querySelector('#fintech')
+        const foodbankDOM = document.querySelector('#foodbank')
+        const eventsDOM = document.querySelector('#events')
         
 
         mainCenterDOM.innerHTML = `
@@ -189,6 +233,244 @@ window.addEventListener('DOMContentLoaded', async() => {
         `
 
 
+        // insurance
+
+        insuranceDOM.innerHTML = `
+        <article class="blog">
+          <div class="blog-img">
+            <img
+              src="${insuranceFilter[insuranceLength].image}"
+              class="main-img img"
+              alt="blog-img"
+            />
+
+            <span class="tag">${insuranceFilter[insuranceLength].category}</span>
+            <a href="/single-blog?id=${insuranceFilter[insuranceLength]._id}" class="tag2 main-btn btn">Read More</a>
+          </div>
+
+          <div class="inner">
+            <a href="/single-blog"
+              ><h3>
+                ${insuranceFilter[insuranceLength].title}
+              </h3></a
+            >
+
+            <div class="info">
+              <p>${insuranceFilter[insuranceLength].user.name}</p>
+              <span><i class="fas fa-comment"></i><span>0</span></span>
+              <p>Nov 16th, 2023</p>
+            </div>
+          </div>
+        </article>
+
+        <article class="blog">
+          <div class="blog-img">
+            <img
+              src="${insuranceFilter[insuranceLength2].image}"
+              class="main-img img"
+              alt="blog-img"
+            />
+
+            <span class="tag">${insuranceFilter[insuranceLength2].category}</span>
+            <a href="/single-blog?id=${insuranceFilter[insuranceLength2]._id}" class="tag2 main-btn btn">Read More</a>
+          </div>
+
+          <div class="inner">
+            <a href="/single-blog"
+              ><h3>
+              ${insuranceFilter[insuranceLength2].title}
+              </h3></a
+            >
+
+            <div class="info">
+              <p>${insuranceFilter[insuranceLength2].user.name}</p>
+              <span><i class="fas fa-comment"></i><span>0</span></span>
+              <p>Nov 16th, 2023</p>
+            </div>
+          </div>
+        </article>
+        `
+
+        // foodbank
+
+         foodbankDOM.innerHTML = `
+         <article class="blog">
+           <div class="blog-img">
+             <img
+               src="${foodbankFilter[foodbankLength].image}"
+               class="main-img img"
+               alt="blog-img"
+             />
+ 
+             <span class="tag">${foodbankFilter[foodbankLength].category}</span>
+             <a href="/single-blog?id=${foodbankFilter[foodbankLength]._id}" class="tag2 main-btn btn">Read More</a>
+           </div>
+ 
+           <div class="inner">
+             <a href="/single-blog"
+               ><h3>
+                 ${foodbankFilter[foodbankLength].title}
+               </h3></a
+             >
+ 
+             <div class="info">
+               <p>${foodbankFilter[foodbankLength].user.name}</p>
+               <span><i class="fas fa-comment"></i><span>0</span></span>
+               <p>Nov 16th, 2023</p>
+             </div>
+           </div>
+         </article>
+ 
+         <article class="blog">
+           <div class="blog-img">
+             <img
+               src="${foodbankFilter[foodbankLength2].image}"
+               class="main-img img"
+               alt="blog-img"
+             />
+ 
+             <span class="tag">${foodbankFilter[foodbankLength2].category}</span>
+             <a href="/single-blog?id=${foodbankFilter[foodbankLength2]._id}" class="tag2 main-btn btn">Read More</a>
+           </div>
+ 
+           <div class="inner">
+             <a href="/single-blog"
+               ><h3>
+               ${foodbankFilter[foodbankLength2].title}
+               </h3></a
+             >
+ 
+             <div class="info">
+               <p>${foodbankFilter[foodbankLength2].user.name}</p>
+               <span><i class="fas fa-comment"></i><span>0</span></span>
+               <p>Nov 16th, 2023</p>
+             </div>
+           </div>
+         </article>
+         `
+
+
+         // fintech
+
+         fintechDOM.innerHTML = `
+         <article class="blog">
+           <div class="blog-img">
+             <img
+               src="${fintechFilter[fintechLength].image}"
+               class="main-img img"
+               alt="blog-img"
+             />
+ 
+             <span class="tag">${foodbankFilter[foodbankLength].category}</span>
+             <a href="/single-blog?id=${fintechFilter[fintechLength]._id}" class="tag2 main-btn btn">Read More</a>
+           </div>
+ 
+           <div class="inner">
+             <a href="/single-blog"
+               ><h3>
+                 ${fintechFilter[fintechLength].title}
+               </h3></a
+             >
+ 
+             <div class="info">
+               <p>${fintechFilter[fintechLength].user.name}</p>
+               <span><i class="fas fa-comment"></i><span>0</span></span>
+               <p>Nov 16th, 2023</p>
+             </div>
+           </div>
+         </article>
+ 
+         <article class="blog">
+           <div class="blog-img">
+             <img
+               src="${fintechFilter[fintechLength2].image}"
+               class="main-img img"
+               alt="blog-img"
+             />
+ 
+             <span class="tag">${fintechFilter[fintechLength2].category}</span>
+             <a href="/single-blog?id=${fintechFilter[fintechLength2]._id}" class="tag2 main-btn btn">Read More</a>
+           </div>
+ 
+           <div class="inner">
+             <a href="/single-blog"
+               ><h3>
+               ${fintechFilter[fintechLength2].title}
+               </h3></a
+             >
+ 
+             <div class="info">
+               <p>${fintechFilter[fintechLength2].user.name}</p>
+               <span><i class="fas fa-comment"></i><span>0</span></span>
+               <p>Nov 16th, 2023</p>
+             </div>
+           </div>
+         </article>
+         `
+
+
+
+         // events
+
+         eventsDOM.innerHTML = `
+         <article class="blog">
+           <div class="blog-img">
+             <img
+               src="${eventsFilter[eventsLength].image}"
+               class="main-img img"
+               alt="blog-img"
+             />
+ 
+             <span class="tag">${foodbankFilter[foodbankLength].category}</span>
+             <a href="/single-blog?id=${eventsFilter[eventsLength]._id}" class="tag2 main-btn btn">Read More</a>
+           </div>
+ 
+           <div class="inner">
+             <a href="/single-blog"
+               ><h3>
+                 ${eventsFilter[eventsLength].title}
+               </h3></a
+             >
+ 
+             <div class="info">
+               <p>${eventsFilter[eventsLength].user.name}</p>
+               <span><i class="fas fa-comment"></i><span>0</span></span>
+               <p>Nov 16th, 2023</p>
+             </div>
+           </div>
+         </article>
+ 
+         <article class="blog">
+           <div class="blog-img">
+             <img
+               src="${eventsFilter[eventsLength2].image}"
+               class="main-img img"
+               alt="blog-img"
+             />
+ 
+             <span class="tag">${eventsFilter[eventsLength2].category}</span>
+             <a href="/single-blog?id=${eventsFilter[eventsLength2]._id}" class="tag2 main-btn btn">Read More</a>
+           </div>
+ 
+           <div class="inner">
+             <a href="/single-blog"
+               ><h3>
+               ${eventsFilter[eventsLength2].title}
+               </h3></a
+             >
+ 
+             <div class="info">
+               <p>${eventsFilter[eventsLength2].user.name}</p>
+               <span><i class="fas fa-comment"></i><span>0</span></span>
+               <p>Nov 16th, 2023</p>
+             </div>
+           </div>
+         </article>
+         `
+
+
+
+ 
        
         
 
