5de291f557e2aca2d280ecf7f1da52b394bcd14c
ebubekeyz <ebubeofforjoe@gmail.com>
2023-11-10 08:08:12 +0100

    fourth

--- public/js/productBlog.js
+++ public/js/productBlog.js
@@ -43,8 +43,103 @@ timeDOM.innerHTML = `<li>${date3}</li>`;
 const submitBtn = document.querySelector('#submit');
 const inputControl = document.querySelector('.input-control');
 
+const searchInput = document.querySelector('#search-input');
+const subBtn = document.querySelector('#submit');
+
+const searchFunc = async () => {
+  try {
+    const response = await fetch(`/api/v1/article`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const data = await response.json();
+    const article = data.article;
+    // console.log(article)
+
+    const eventFilter = article.filter((event) => {
+      if (event.category === 'products') {
+        return event;
+      }
+    });
+
+    const value = searchInput.value;
+    if (value) {
+      const searchStore = eventFilter.filter((search) => {
+        let { title } = search;
+        title = title.toLowerCase();
+        if (title.startsWith(value)) {
+          return search;
+        }
+      });
+
+      console.log(searchStore);
+      const eventDOM = document.querySelector('#products');
+
+      eventDOM.innerHTML = searchStore
+        .map((searchMap) => {
+          let {
+            _id: id,
+            title,
+            description,
+            image,
+            category,
+            user: { name: name },
+            createdAt,
+          } = searchMap;
+
+          let eventDate = new Date(createdAt);
+
+          let eventMonth = eventDate.getMonth();
+          eventMonth = months[eventMonth];
+          const eventYear = eventDate.getFullYear();
+          const eventDay = eventDate.getDate();
+          console.log(eventMonth, eventDay, eventYear);
+
+          return `
+      <article class="blog">
+      <div class="blog-img">
+        <img
+          src="${image}"
+          class="main-img img"
+          alt="blog-img"
+        />
+
+        <span class="tag">${category.toUpperCase()}</span>
+        <a href="/single-article?id=${id}" class="tag2 main-btn btn">Read More</a>
+      </div>
+
+      <div class="inner">
+        <a href="/single-article?id=${id}"
+          ><h3>
+            ${title}
+          </h3></a
+        >
+
+        <div class="info">
+          <p>${name}</p>
+          <p>${eventMonth} ${eventDay}, ${eventYear}</p>
+        </div>
+      </div>
+    </article>
+      `;
+        })
+        .join();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+searchFunc();
+
+searchInput.addEventListener('keyup', searchFunc);
+
 submitBtn.addEventListener('click', () => {
   inputControl.classList.add('show');
+  submitBtn.style.display = 'none'
 });
 
 window.addEventListener('DOMContentLoaded', async () => {
